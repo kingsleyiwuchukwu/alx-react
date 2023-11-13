210df6b4145382b1d318d777e49631d3f5367adc
Kingsley Iwuchukwu <kingsleyiwuchukwu0@gmail.com>
2023-11-13 23:15:53 +0100

    React intro

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './holberton-logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Welcome!.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="Holberton Logo" />
+        <h1>School dashboard</h1>
       </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <footer className="App-footer">
+        <p>Copyright 2020 - Holberton School</p>
+      </footer>
     </div>
   );
 }
