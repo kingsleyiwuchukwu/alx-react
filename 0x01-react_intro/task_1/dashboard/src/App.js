9d7ea55b43b9fc2c39f2fd9d759d6c416232e4c2
kingsleyiwuchukwu <kingsleyiwuchukwu0@gmail.com>
2023-11-15 23:40:50 +0100

    code fix

--- 0x01-react_intro/task_1/dashboard/src/App.js
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import logo from './holberton-logo.jpg';
-import './App.css';
+import logo from "./holberton-logo.jpg";
+import "./App.css";
+import { getFullYear, getFooterCopy } from "./utils";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="Holberton Logo" />
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
-      </header>
+      </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
-      <footer className="App-footer">
-        <p>Copyright 2020 - Holberton School</p>
-      </footer>
+      <div className="App-footer">
+        Copyright {getFullYear()} - {getFooterCopy()}
+      </div>
     </div>
   );
 }
