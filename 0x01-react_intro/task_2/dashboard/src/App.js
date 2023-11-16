f3af4738e57d107de64fbfb95a1a7436238aac68
kingsleyiwuchukwu <kingsleyiwuchukwu0@gmail.com>
2023-11-16 08:18:20 +0100

    Modify the Notifications

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -1,22 +1,26 @@
-import logo from "./holberton-logo.jpg";
-import "./App.css";
-import { getFullYear, getFooterCopy } from "./utils";
+import React from "react";
+import "./Notifications.css";
+import closeIcon from "./close-icon.png";
+import { getLatestNotification } from "./utils";
 
-function App() {
+function Notifications() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>School dashboard</h1>
-      </div>
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
-      </div>
-      <div className="App-footer">
-        Copyright {getFullYear()} - {getFooterCopy()}
-      </div>
+    <div className="Notifications">
+      <button
+        style={{ color: "#3a3a3a", fontWeight: "bold", background: "none", border: "none", fontSize: "15px", position: "absolute", right: "2px", top: "2px", cursor: "pointer" }}
+        aria-label="Close"
+        onClick={console.log("Close button has been clicked")}
+      >
+        <img src={closeIcon} alt="closeIcon" width="10px" />
+      </button>
+      <p>Here is the list of notifications</p>
+      <ul>
+        <li data="default">New course available</li>
+        <li data="urgent">New resume available</li>
+        <li data="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default Notifications;
